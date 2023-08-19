e38be62155cc179338c8b3455886524b39020ab4
Doug Turnbull <softwaredoug@gmail.com>
2023-08-19 17:08:25 -0400

    Rename repo

--- ann_benchmarks/algorithms/naive_lsh/module.py
+++ ann_benchmarks/algorithms/naive_lsh/module.py
@@ -1,4 +1,4 @@
-import similarities.lsh  # My LSH implementation, see dockerfile for installation
+import np_sims.lsh  # My LSH implementation, see dockerfile for installation
 
 from ..base.module import BaseANN
 
@@ -11,17 +11,24 @@ class NaiveLSH(BaseANN):
     # Here LSH models cosine similarity, so angular is the only thing we've specified
     # in config.yml to run this on
     def __init__(self, metric, index_params):
+        # These are params about how to build the index passed from config.yml
+        #     run_groups:
+        #       test:
+        #         arg_groups: [{num_projections: 128, num_projections: 512, num_projections: 1024}]
+
+        self.num_projections = index_params['num_projections']
         super().__init__()
 
     # "Fit" or index the data, whatever your parlance you prefer :)
     def fit(self, X, y=None):
         vectors = X
-        hashes, projs = similarities.lsh.load_or_build_index(vectors)
+        self.projs = np_sims.lsh.create_projections(num_projections=self.num_projections, dims=vectors.shape[1])
+        self.hashes = np_sims.lsh.index(vectors, self.projs)
         return self
 
     # Given query vector v, return n nearest neighbors
     def query(self, v, n):
-        return self.lsh.query(v, n)
+        return np_sims.lsh.query(v, self.hashes, self.projs)[0]
 
     # Return the name of the algorithm
     def __str__(self):
